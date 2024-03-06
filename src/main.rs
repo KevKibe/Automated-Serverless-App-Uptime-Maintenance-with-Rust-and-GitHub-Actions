949f95ea4afb2c8fa19f8dce942c16cee688228e
KevKibe <kchegz234@gmail.com>
2024-03-06 14:52:20 +0300

    added more endpoints

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use reqwest::Error;
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let urls = vec![
-        "https://insightai.onrender.com/",
-        "https://insightaiserver.onrender.com/health",
         "https://aipredictivecare-backend-services.onrender.com",
         "https://clean-waste-backend.onrender.com",
         "https://debtscleared-backend.onrender.com",
         "https://insightai-backend-eetx.onrender.com",
-        "https://insightaiserver-z4ms.onrender.com/health"
+        "https://insightaiserver-z4ms.onrender.com/health",
+        "https://africantrade-backend.onrender.com",
+        "https://headhuntrs-backend.onrender.com"
+
 
     ];
 
